5322ac56ba64ecf87c1bd5a959608ec400c7683c
Sachin-Engineer <sachin.engineer.22@gmail.com>
2026-01-29 16:42:18 +0530

    Fix: something

--- src/components/BlogsSection.jsx
+++ src/components/BlogsSection.jsx
@@ -33,8 +33,8 @@ const BlogsSection = () => {
     <section className="py-12 sm:py-16 md:py-20 max-w-[1440px] mx-auto">
       <div className="text-center mb-12 md:mb-15">
         <h2 className="text-3xl md:text-4xl lg:text-5xl font-semibold text-gray-900 mb-4">Blogs</h2>
-        <p className="text-justify text-sm md:text-base text-gray-500 leading-relaxed max-w-3xl mx-auto">
-          Stay up to date with the latest trends and insights in technology with our informative blog.<br />
+        <p className="text-justify text-sm md:text-base text-gray-500 leading-relaxed max-w-[90%] mx-auto text-center flex justify-center">
+          Stay up to date with the latest trends and insights in technology with our informative blog.
           From industry news to expert tips, we have got you covered.
         </p>
       </div>
